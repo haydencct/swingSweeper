74043540e22b4978e1e5c5edaa535f327bf38beb
Hayden C <haychezzz@gmail.com>
2019-01-11 09:42:01 -0500

    the files

---
+++ Map.java
@@ -0,0 +1,31 @@
+package swingSweeper;
+
+import in_out.Out;
+
+public class Map {
+	public static int[][] generateMap(double mineRate, int mapWidth, int mapHeight) {
+		boolean[][] mines = new boolean[mapWidth + 2][mapHeight + 2];
+		for (int a = 0; a < mapWidth; a++)
+			for (int b = 0; b < mapHeight; b++)
+				mines[a][b] = (Math.random() < mineRate / 100);
+		
+		int[][] adj = new int[mapWidth][mapHeight];
+		for (int a = 0; a < mapWidth; a++) {
+			for (int b = 0; b < mapHeight; b++) {
+				for (int aa = a - 1; aa <= a + 1; aa++) {
+					for (int bb = b - 1; bb <= b + 1; bb++) {
+						if (aa < 0 || bb < 0) {
+						} else {
+							if (mines[aa][bb]) {
+								adj[a][b]++;
+								adj[aa][bb] = 9;
+							}
+						}}}}}
+		for (int a = 0; a < mapWidth; a++) {
+			for (int b = 0; b < mapHeight; b++) {
+				Out.print(adj[a][b]);
+			}
+			Out.println();		
+		}
+		return adj;
+}}
---
+++ Menu.java
@@ -0,0 +1,60 @@
+package swingSweeper;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Menu {
+	public static void main(String[] args) {
+		MineFrame frame = new MineFrame();
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+}
+
+class MineFrame extends JFrame implements ActionListener {
+	static final long serialVersionUID = 1L;
+	private JTextField mineRate, mapWidth, mapHeight;
+
+	public MineFrame() {
+		super("Minesweeper Setup");
+		// organize content pane
+		JPanel pane = (JPanel) getContentPane();
+		pane.setLayout(new BorderLayout());
+		// create fields for i/oput
+		mineRate = new JTextField(10);
+		mapWidth = new JTextField(10);
+		mapHeight = new JTextField(10);
+		// add labelled input fields to display
+		JPanel inFieldPane = new JPanel();
+		inFieldPane.setLayout(new GridLayout(3, 2));
+		inFieldPane.add(new JLabel("Mine Rate (0-100)"));
+		inFieldPane.add(mineRate);
+		mineRate.addActionListener(this);
+		inFieldPane.add(new JLabel("Map Width"));
+		inFieldPane.add(mapWidth);
+		mapWidth.addActionListener(this);
+		inFieldPane.add(new JLabel("Map Height"));
+		inFieldPane.add(mapHeight);
+		mapHeight.addActionListener(this);
+		pane.add(inFieldPane, BorderLayout.NORTH);
+		// add submission button
+		JPanel submitPane = new JPanel();
+		submitPane.setLayout(new FlowLayout());
+		JButton submitButton = new JButton("Play");
+		submitButton.addActionListener(this);
+		submitPane.add(submitButton);
+		pane.add(submitPane, BorderLayout.CENTER);
+		pack();
+		setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// this line is pretty long, but it just turns all the user inputs from strings to integers in one line, then gives them to the method generateMap within the class Map.
+			int[][]map = Map.generateMap((Double.parseDouble(mineRate.getText())), Integer.parseInt(mapWidth.getText()), Integer.parseInt(mapHeight.getText()));
+			Window.makeGame(map);
+		}
+	}
